d5135ea7d6b719d754cc58bcde71e3bbddc87493
nmsteve <nmsteve45@gmail.com>
2023-10-10 13:57:03 +0300

    Dc contract v1 and tests

---
+++ hardhat.config.js
@@ -0,0 +1,60 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.SEPOLIA,
+        // allowUnlimitedContractSize: true,
+        // timeout: 90000,
+        blockNumber: 4462147,
+        // chainId: 1,
+        // gas: 9000000000000000
+      }
+    },
+   
+    sepolia: {
+      url: process.env.SEPOLIA,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 10,
+        passphrase: "",
+      },
+    },
+    opt: {
+      url: process.env.OPTIMISM,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 10,
+        passphrase: "",
+      },
+
+    },
+    arb: {
+      url: process.env.ARBITRUM,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 10,
+        passphrase: "",
+      },
+
+    },
+
+  },
+
+  etherscan: {
+
+    apiKey: process.env.ETHERSCAN_API_KEY,
+    //apiKey: process.env.BSCSCAN_API_KEY
+    //apiKey: process.env.ARBITRUM_API_KEY
+  },
+};
