f419ded7dd56a610b4ff04a6e00920af1a72f5d7
nmsteve <nmsteve45@gmail.com>
2023-10-24 12:52:10 +0300

    added functions & tests:`removeUser` &`updateUser`

--- contracts/admin.sol
+++ contracts/admin.sol
@@ -13,9 +13,11 @@ contract AdminContract {
     uint256 public nextUserId = 1;
 
     event UserCreated(uint256 userId, address userAddress);
+    event UserRemoved(uint256 userId);
+    event UserUpdated(uint256 userId, address newUserAddress, string newPasswordHash);
 
     constructor() {
-        admin = msg.sender;
+        admin = msg.sender; // Initialize the admin as the contract creator
     }
 
     modifier onlyAdmin() {
@@ -23,6 +25,11 @@ contract AdminContract {
         _;
     }
 
+    /**
+     * @dev Add a new user with the provided address and password hash.
+     * @param _userAddress The user's Ethereum address.
+     * @param _passwordHash The user's password hash.
+     */
     function addUser(address _userAddress, string memory _passwordHash) public onlyAdmin {
         require(_userAddress != address(0), "Invalid user address");
         require(bytes(_passwordHash).length > 0, "Password hash cannot be empty");
@@ -32,9 +39,42 @@ contract AdminContract {
         nextUserId++;
     }
 
+    /**
+     * @dev Get the user's address and password hash by their user ID.
+     * @param _userId The ID of the user.
+     * @return The user's Ethereum address and password hash.
+     */
     function getUser(uint256 _userId) public view returns (address, string memory) {
         require(_userId > 0 && _userId < nextUserId, "User does not exist");
         User storage user = users[_userId];
         return (user.userAddress, user.passwordHash);
+    }
+
+    /**
+     * @dev Remove a user by their user ID.
+     * @param _userId The ID of the user to remove.
+     */
+    function removeUser(uint256 _userId) public onlyAdmin {
+        require(_userId > 0 && _userId < nextUserId, "User does not exist");
+        emit UserRemoved(_userId);
+        delete users[_userId];
+    }
+
+    /**
+     * @dev Update the user's address and password hash by their user ID.
+     * @param _userId The ID of the user to update.
+     * @param _newUserAddress The new user's Ethereum address.
+     * @param _newPasswordHash The new password hash.
+     */
+    function updateUser(uint256 _userId, address _newUserAddress, string memory _newPasswordHash) public onlyAdmin {
+        require(_userId > 0 && _userId < nextUserId, "User does not exist");
+        require(_newUserAddress != address(0), "Invalid user address");
+        require(bytes(_newPasswordHash).length > 0, "Password hash cannot be empty");
+
+        User storage user = users[_userId];
+        user.userAddress = _newUserAddress;
+        user.passwordHash = _newPasswordHash;
+
+        emit UserUpdated(_userId, _newUserAddress, _newPasswordHash);
     }
 }
--- test/admin.js
+++ test/admin.js
@@ -4,70 +4,193 @@ const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helper
 
 const { BigNumber } = require('ethers');
 
-describe.only('AdminContract', function () {
+describe('AdminContract', function () {
     let adminContract;
     let admin;
 
     async function deploy() {
-        const [owner, authorizedUser1, authorizedUser2, addr1, addr2, ...addrs] = await ethers.getSigners();
+        const [owner, user1, user2, addr1, addr2, ...addrs] = await ethers.getSigners();
 
         const AdminContract = await ethers.getContractFactory('AdminContract');
         adminContract = await AdminContract.deploy();
-        return { adminContract, admin: owner , addr1};
+        return { adminContract, admin: owner , addr1,user1, user2 };
     }
 
     beforeEach(async function () {
-        ({ adminContract, admin, addr1  } = await loadFixture(deploy));
+        ({ adminContract, admin, addr1, user1, user2  } = await loadFixture(deploy));
     });
 
-    it('should allow the admin to add a user', async function () {
-        const userAddress = ethers.Wallet.createRandom().address;
-        const passwordHash = 'password123';
+    describe('Add user', function () {
+        it('should allow the admin to add a user', async function () {
+            const userAddress = ethers.Wallet.createRandom().address;
+            const passwordHash = 'password123';
 
-        await adminContract.addUser(userAddress, passwordHash);
+            await adminContract.addUser(userAddress, passwordHash);
 
-        const [fetchedUserAddress, fetchedPasswordHash] = await adminContract.getUser(1);
+            const [fetchedUserAddress, fetchedPasswordHash] = await adminContract.getUser(1);
 
-        expect(fetchedUserAddress).to.equal(userAddress);
-        expect(fetchedPasswordHash).to.equal(passwordHash);
+            expect(fetchedUserAddress).to.equal(userAddress);
+            expect(fetchedPasswordHash).to.equal(passwordHash);
+        });
+
+        it('should not allow a non-admin to add a user', async function () {
+            const userAddress = ethers.Wallet.createRandom().address;
+            const passwordHash = 'password123';
+
+            await expect(
+                adminContract.connect(addr1).addUser(userAddress, passwordHash)
+            ).to.be.revertedWith('Only admin can perform this action');
+        });
+
+        it('should not allow an admin to add a user with an empty address', async function () {
+            const passwordHash = 'password123';
+
+            await expect(adminContract.addUser(ethers.ZeroAddress, passwordHash)).to.be.revertedWith(
+                'Invalid user address'
+            );
+        });
+        it('should not allow an admin to add a user with an empty password hash', async function () {
+            const userAddress = ethers.Wallet.createRandom().address;
+
+            await expect(adminContract.addUser(userAddress, '')).to.be.revertedWith('Password hash cannot be empty');
+        });
+    })
+
+    describe('Get user', function () {
+        it('should return user data based on user ID', async function () {
+            const userAddress = ethers.Wallet.createRandom().address;
+            const passwordHash = 'password123';
+
+            await adminContract.addUser(userAddress, passwordHash);
+
+            const [fetchedUserAddress, fetchedPasswordHash] = await adminContract.getUser(1);
+
+            expect(fetchedUserAddress).to.equal(userAddress);
+            expect(fetchedPasswordHash).to.equal(passwordHash);
+        });
+
+        it('should not return data for a non-existent user', async function () {
+            await expect(adminContract.getUser(2)).to.be.revertedWith('User does not exist');
+        });
+    })
+
+    describe('Update user', function () {
+       
+        it('should update user address and password hash', async function () {
+           
+
+            // Add a user
+            await adminContract.addUser(user1.address, 'oldPasswordHash');
+
+            const userId = 1;
+            const newUserAddress = user1.address;
+            const newPasswordHash = 'newPasswordHash';
+
+            // Update the user
+            await adminContract.updateUser(userId, newUserAddress, newPasswordHash);
+
+            // Get the user's updated details
+            const [updatedUserAddress, updatedPasswordHash] = await adminContract.getUser(userId);
+
+            expect(updatedUserAddress).to.equal(newUserAddress);
+            expect(updatedPasswordHash).to.equal(newPasswordHash);
+        });
+
+        it('should not update a user by non-admin', async function () {
+            const { adminContract, user1, user2 } = await loadFixture(deploy);
+
+            await adminContract.addUser(user1.address, 'passwordHash1');
+            await expect(adminContract.connect(user2).updateUser(1, user2.address, 'newPasswordHash')).to.be.revertedWith("Only admin can perform this action");
+        });
+
+        it('should not update user with invalid user ID', async function () {
+          
+
+            const invalidUserId = 0; // Invalid user ID
+            const newUserAddress = user1.address;
+            const newPasswordHash = 'newPasswordHash';
+
+            // Attempt to update an invalid user
+            await expect(adminContract.updateUser(invalidUserId, newUserAddress, newPasswordHash)).to.be.revertedWith('User does not exist');
+        });
+
+        it('should not update user with invalid user address', async function () {
+           
+
+            // Add a user
+            await adminContract.addUser(user1.address, 'oldPasswordHash');
+
+            const userId = 1;
+            const invalidUserAddress = ethers.ZeroAddress; // Invalid user address
+            const newPasswordHash = 'newPasswordHash';
+
+            // Attempt to update with an invalid user address
+            await expect(adminContract.updateUser(userId, invalidUserAddress, newPasswordHash)).to.be.revertedWith('Invalid user address');
+        });
+
+        it('should not update user with empty password hash', async function () {
+        
+
+            // Add a user
+            await adminContract.addUser(user1.address, 'oldPasswordHash');
+
+            const userId = 1;
+            const newUserAddress = user1.address;
+            const emptyPasswordHash = '';
+
+            // Attempt to update with an empty password hash
+            await expect(adminContract.updateUser(userId, newUserAddress, emptyPasswordHash)).to.be.revertedWith('Password hash cannot be empty');
+        });
+
     });
 
-    it('should not allow a non-admin to add a user', async function () {
-        const userAddress = ethers.Wallet.createRandom().address;
-        const passwordHash = 'password123';
+    describe('Remove user', function () {
+        it('should remove a user', async function () {
+            const { adminContract, admin } = await loadFixture(deploy);
 
-        await expect(
-            adminContract.connect(addr1).addUser(userAddress, passwordHash)
-        ).to.be.revertedWith('Only admin can perform this action');
-    });
+            // Add a new user
+            const newUserAddress = ethers.getAddress('0x742d35Cc6634C0532925a3b844Bc454e4438f44e');
+            const newPasswordHash = '0x5efcc5e4d707d5a2253246c37aebadd38bfda096';
+            await adminContract.addUser(newUserAddress, newPasswordHash);
 
-    it('should return user data based on user ID', async function () {
-        const userAddress = ethers.Wallet.createRandom().address;
-        const passwordHash = 'password123';
+            // Get the user ID
+            const userId = 1;
 
-        await adminContract.addUser(userAddress, passwordHash);
+            // Ensure the user was added
+            const [userAddress, passwordHash] = await adminContract.getUser(userId);
+            expect(userAddress).to.equal(newUserAddress);
+            expect(passwordHash).to.equal(newPasswordHash);
 
-        const [fetchedUserAddress, fetchedPasswordHash] = await adminContract.getUser(1);
+            // Remove the user
+            await adminContract.removeUser(userId);
 
-        expect(fetchedUserAddress).to.equal(userAddress);
-        expect(fetchedPasswordHash).to.equal(passwordHash);
-    });
+            // Check that the user is removed
+            const [removedUserAddress, removedPasswordHash] = await adminContract.getUser(userId);
+            expect(removedUserAddress).to.equal(ethers.ZeroAddress); // Address should be zero
+            expect(removedPasswordHash).to.equal(''); // Password hash should be empty
+        });
+        it('should not remove a user by non-admin', async function () {
+            const address = ethers.Wallet.createRandom().address
+            const hash = 'xxxvht905n;bmjl;kfyv'
 
-    it('should not return data for a non-existent user', async function () {
-        await expect(adminContract.getUser(2)).to.be.revertedWith('User does not exist');
-    });
+            //add user
+            await adminContract.addUser(address, hash)
 
-    it('should not allow an admin to add a user with an empty address', async function () {
-        const passwordHash = 'password123';
+            //confirm user is added
+            const [addedAddress, addHash] = await adminContract.getUser(1)
+            expect(addedAddress).to.be.equals(address)
+            expect(addHash).to.be.eqls(hash)
 
-        await expect(adminContract.addUser(ethers.ZeroAddress, passwordHash)).to.be.revertedWith(
-            'Invalid user address'
-        );
-    });
+            //try to remove by a non-admin
+           await expect(adminContract.connect(user1).removeUser(1)).to.be.revertedWith("Only admin can perform this action")
+        })
+        it('should revert when trying to remove a non-existent user', async function () {
+            const { adminContract, admin } = await loadFixture(deploy);
 
-    it('should not allow an admin to add a user with an empty password hash', async function () {
-        const userAddress = ethers.Wallet.createRandom().address;
+            // Attempt to remove a non-existent user with ID 1
+            const userId = 1;
+            await expect(adminContract.removeUser(userId)).to.be.revertedWith('User does not exist');
+        });
+    })
 
-        await expect(adminContract.addUser(userAddress, '')).to.be.revertedWith('Password hash cannot be empty');
-    });
 });
