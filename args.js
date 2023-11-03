eb396063329b98194f0bf74495366a83440af0c8
nmsteve <nmsteve45@gmail.com>
2023-11-03 20:57:49 +0300

    args for deploying case contract

---
+++ args.js
@@ -0,0 +1,15 @@
+
+
+args = [
+    '0x6A60F5b0875a44E11aE9eca0413b01961F089056',
+    [
+    'Identification',
+    'Collection',
+    'Acquisition',
+    'Preservation',
+    'Analysis',
+    'Presentation',
+    'Archiving',
+]]
+
+module.exports = args
