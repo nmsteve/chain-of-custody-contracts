551474b22e15d65962d8a5bdefd44199347334f9
nmsteve <nmsteve45@gmail.com>
2023-11-15 15:14:37 +0300

    update to include isActive and count from 0

---
+++ contracts/accounts.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Accounts {
+    address public admin;
+
+    struct User {
+        address userAddress;
+        string passwordHash;
+        bool isActive;
+    }
+
+    mapping(uint256 => User) public users;
+    uint256 public nextUserId = 0;
+
+    event UserCreated(uint256 userId, address userAddress);
+    event UserUpdated(uint256 userId, string newPasswordHash);
+    event UserStateSet(bool userState);
+
+    constructor() {
+        admin = msg.sender; // Initialize the admin as the contract creator
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can perform this action");
+        _;
+    }
+
+    /**
+     * @dev Add a new user with the provided address and password hash.
+     * @param _userAddress The user's Ethereum address.
+     * @param _passwordHash The user's password hash.
+     */
+    function addUser(
+        address _userAddress,
+        string memory _passwordHash
+    ) public onlyAdmin {
+        require(_userAddress != address(0), "Invalid user address");
+        require(
+            bytes(_passwordHash).length > 0,
+            "Password hash cannot be empty"
+        );
+
+        users[nextUserId] = User(_userAddress, _passwordHash, true);
+        emit UserCreated(nextUserId, _userAddress);
+        nextUserId++;
+    }
+
+    /**
+     * @dev Get the user's address and password hash by their user ID.
+     * @param _userId The ID of the user.
+     * @return The user's Ethereum address and password hash.
+     */
+    function getUser(
+        uint256 _userId
+    ) public view returns (address, string memory, bool) {
+        require(_userId < nextUserId, "User does not exist");
+        User storage user = users[_userId];
+        return (user.userAddress, user.passwordHash, user.isActive);
+    }
+
+    /**
+     * @dev set a user state to active or inactive.
+     * @param _userId The ID of the user to set state
+     * @param _state The state to set  true or false
+     */
+    function setUserState(uint256 _userId, bool _state) public onlyAdmin {
+        require( _userId < nextUserId, "User does not exist");
+        User storage user = users[_userId];
+        user.isActive = _state;
+        emit UserStateSet(_state);
+    }
+
+    /**
+     * @dev Update the user's address and password hash by their user ID.
+     * @param _userId The ID of the user to update.
+     * @param _newPasswordHash The new password hash.
+     */
+    function updateUser(
+        uint256 _userId,
+        string memory _newPasswordHash
+    ) public onlyAdmin {
+        require( _userId < nextUserId, "User does not exist");
+        require(
+            bytes(_newPasswordHash).length > 0,
+            "Password hash cannot be empty"
+        );
+
+        User storage user = users[_userId];
+        user.passwordHash = _newPasswordHash;
+
+        emit UserUpdated(_userId, _newPasswordHash);
+    }
+
+    /**
+     * @dev Get all user IDs and addresses.
+     * @return Arrays of user addresses.
+     */
+    function getAllUsers()
+        public
+        view
+        returns (address[] memory)
+    {
+       
+        address[] memory userAddresses = new address[](nextUserId);
+
+        for (uint256 i = 0; i < nextUserId; i++) {
+          
+            userAddresses[i] = users[i].userAddress;
+        }
+
+        return (userAddresses);
+    }
+}
---
+++ test/accounts.js
@@ -0,0 +1,226 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+
+describe.only('Contract', function () {
+    let accountsContract;
+    let admin;
+
+    async function deploy() {
+        const [owner, user1, user2, user3, user4, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+        const AdminContract = await ethers.getContractFactory('Accounts');
+        accountsContract = await AdminContract.deploy();
+        return { accountsContract, admin: owner, addr1, user1, user2, user3, user4 };
+    }
+
+    beforeEach(async function () {
+        ({ accountsContract, admin, addr1, user1, user2, user3, user4 } = await loadFixture(deploy));
+    });
+
+    describe('Add user', function () {
+        it('should allow the admin to add a user', async function () {
+            const userAddress = ethers.Wallet.createRandom().address;
+            const passwordHash = 'password123';
+
+            await accountsContract.addUser(userAddress, passwordHash);
+
+            const [fetchedUserAddress, fetchedPasswordHash] = await accountsContract.getUser(0);
+
+            expect(fetchedUserAddress).to.equal(userAddress);
+            expect(fetchedPasswordHash).to.equal(passwordHash);
+        });
+
+        it('should not allow a non-admin to add a user', async function () {
+            const userAddress = ethers.Wallet.createRandom().address;
+            const passwordHash = 'password123';
+
+            await expect(
+                accountsContract.connect(addr1).addUser(userAddress, passwordHash)
+            ).to.be.revertedWith('Only admin can perform this action');
+        });
+
+        it('should not allow an admin to add a user with an empty address', async function () {
+            const passwordHash = 'password123';
+
+            await expect(accountsContract.addUser(ethers.ZeroAddress, passwordHash)).to.be.revertedWith(
+                'Invalid user address'
+            );
+        });
+        it('should not allow an admin to add a user with an empty password hash', async function () {
+            const userAddress = ethers.Wallet.createRandom().address;
+
+            await expect(accountsContract.addUser(userAddress, '')).to.be.revertedWith('Password hash cannot be empty');
+        });
+    })
+
+    describe('Get a user', function () {
+        it('should return user data based on user ID', async function () {
+            const userAddress = ethers.Wallet.createRandom().address;
+            const passwordHash = 'password123';
+
+            await accountsContract.addUser(userAddress, passwordHash);
+
+            const [fetchedUserAddress, fetchedPasswordHash, fetchedActiveState] = await accountsContract.getUser(0);
+
+            expect(fetchedUserAddress).to.equal(userAddress);
+            expect(fetchedPasswordHash).to.equal(passwordHash);
+            expect(fetchedActiveState).to.equal(true)
+        });
+
+        it('should not return data for a non-existent user', async function () {
+            await expect(accountsContract.getUser(2)).to.be.revertedWith('User does not exist');
+        });
+
+
+    })
+
+    describe('Update user', function () {
+
+        it('should update user  password hash', async function () {
+
+            // Add a user
+            await accountsContract.addUser(user1.address, 'oldPasswordHash');
+
+            const userId = 0;
+            const newPasswordHash = 'newPasswordHash';
+
+            // Update the user
+            await accountsContract.updateUser(userId, newPasswordHash);
+
+            // Get the user's updated details
+            const [userAddress, updatedPasswordHash, state] = await accountsContract.getUser(userId);
+
+
+            expect(updatedPasswordHash).to.equal(newPasswordHash);
+        });
+
+        it('should not update a user by non-admin', async function () {
+            const { accountsContract, user1, user2 } = await loadFixture(deploy);
+
+            await accountsContract.addUser(user1.address, 'passwordHash1');
+            await expect(accountsContract.connect(user2).updateUser(0, 'newPasswordHash')).to.be.revertedWith("Only admin can perform this action");
+        });
+
+        it('should not update user with invalid user ID', async function () {
+
+
+            const invalidUserId = 0; // Invalid user ID
+            const newUserAddress = user1.address;
+            const newPasswordHash = 'newPasswordHash';
+
+            // Attempt to update an invalid user
+            await expect(accountsContract.updateUser(invalidUserId, newPasswordHash)).to.be.revertedWith('User does not exist');
+        });
+
+        it('should not update user with empty password hash', async function () {
+
+
+            // Add a user
+            await accountsContract.addUser(user1.address, 'oldPasswordHash');
+
+            const userId = 0;
+            const emptyPasswordHash = '';
+
+            // Attempt to update with an empty password hash
+            await expect(accountsContract.updateUser(userId, emptyPasswordHash)).to.be.revertedWith('Password hash cannot be empty');
+        });
+
+        it('should revert when called by a non-admin', async () => {
+            const nonAdminAddress = user1.address// the address of a non-admin user
+            const userId = 0; // Replace with the desired user ID
+            const newPasswordHash = 'newPassword123'; // Replace with the desired new password hash
+
+            // Connect a non-admin wallet to the contract
+            const nonAdminAccountsContract = accountsContract.connect(user1);
+
+            // Expect the updateUser function to revert when called by a non-admin
+            await expect(nonAdminAccountsContract.updateUser(userId, newPasswordHash)).to.be.revertedWith('Only admin can perform this action');
+        });
+
+    });
+
+    describe.only('get all users', function () {
+
+        it("should return empty arrays when no users are added", async () => {
+            const userAddresses = await accountsContract.getAllUsers();
+            expect(userAddresses).to.be.an("array").that.is.empty;
+        });
+
+        it("should return user IDs and addresses after adding users", async () => {
+            // Add a few users
+            await accountsContract.addUser(user1.address, "password1");
+            await accountsContract.addUser(user2.address, "password2");
+            await accountsContract.addUser(user3.address, "password2");
+            await accountsContract.addUser(user4.address, "password2");
+
+            const userAddresses = await accountsContract.getAllUsers();
+            console.log(userAddresses)
+
+            // Check if the arrays contain the correct user IDs and addresses
+            expect(userAddresses).to.deep.equal([user1.address, user2.address, user3.address, user4.address]);
+        });
+
+    })
+
+
+    describe('setUserState', () => {
+
+        it('should set a user state to active', async () => {
+            await accountsContract.addUser(user1.address, 'PasswordHash');
+            const userId = 0; // The desired user ID
+            const newState = true;
+
+            // Call the setUserState function
+            await accountsContract.setUserState(userId, newState);
+
+            // Fetch the user to check the updated state
+            const [userAddress, passwordHash, isActive] = await accountsContract.getUser(userId);
+
+            // Assert that the user state is updated
+            expect(isActive).to.equal(newState);
+        });
+
+        it('should set a user state to inactive', async () => {
+            await accountsContract.addUser(user1.address, 'PasswordHash');
+
+            const userId = 0; // Replace with another user ID
+            const newState = false;
+
+            // Call the setUserState function
+            await accountsContract.setUserState(userId, newState);
+
+            // Fetch the user to check the updated state
+            const [userAddress, passwordHash, isActive] = await accountsContract.getUser(userId);
+
+            // Assert that the user state is updated
+            expect(isActive).to.equal(newState);
+        });
+
+        it('should revert if the user does not exist', async () => {
+
+            const nonExistentUserId = 100; // An ID that does not exist
+
+            // Expect the setUserState function to revert
+            await expect(accountsContract.setUserState(nonExistentUserId, true)).to.be.revertedWith('User does not exist');
+        });
+
+        it('should revert when called by a non-admin', async () => {
+
+            await accountsContract.addUser(user1.address, 'PasswordHash');
+
+            const userId = 0; // Replace with the desired user ID
+            const newState = true;
+
+            // Connect a non-admin wallet to the contract
+
+            const nonAdminAccountsContract = accountsContract.connect(user1);
+
+            // Expect the setUserState function to revert when called by a non-admin
+            await expect(nonAdminAccountsContract.setUserState(userId, newState)).to.be.revertedWith('Only admin can perform this action');
+        });
+
+    });
+
+
+});
